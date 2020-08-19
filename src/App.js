d9ef6b527399277ebe5e643146ec3ed266a3de8c
katiadobra <broun.kat@gmail.com>
2020-08-19 15:41:15 +0200

    Adding Components Form and Weather

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import Form from './components/Form';
-import Weather from './components/Weather';
+import Form from './components/Form/Form';
+import Weather from './components/Weather/Weather';
+import Titles from './components/Titles/Titles';
+import './App.scss';
 
 function App() {
   const [weather, setWeather] = useState([]);
@@ -10,37 +12,78 @@ function App() {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
+
     e.preventDefault();
     const apiData = 
       await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${APIKEY}`)
       .then(res => res.json())
       .then(data => data)
-    
-      setWeather({
-        data: apiData,
-        city: apiData.city,
-        country: apiData.sys.country,
-        description: apiData.weather[0].description,
-        temperature: apiData.main.temp,
-        error: ""
-      })
+
+      let descr = apiData.weather[0].description;
+      let weatherIcon = '';
+
+      if (descr.includes('sun')) {
+        weatherIcon = 'sunny';
+      } else if (descr.includes('clouds')) {
+        weatherIcon = 'cloudy';
+      } else if (descr.includes('rain')) {
+        weatherIcon = 'rainy';
+      } else if (descr.includes('starry')) {
+        weatherIcon = 'starry';
+      } else if (descr.includes('snow')) {
+        weatherIcon = 'snowy';
+      } else if (descr.includes('storm')) {
+        weatherIcon = 'stormy';
+      }
+
+      if (city && country) {
+        setWeather({
+          data: apiData,
+          city: apiData.city,
+          country: apiData.sys.country,
+          description: apiData.weather[0].description,
+          temperature: Math.round(apiData.main.temp),
+          icon: weatherIcon,
+          error: ""
+        })
+      } else {
+        setWeather({
+          data: '',
+          city: '',
+          country: '',
+          description: '',
+          temperature: '',
+          icon: '',
+          error: "Please type a city and country"
+        })
+      }
   }
 
   return (
     <div className="App">
-      <header>
-        <h3>Weather App</h3>
-      </header>
-      <Form getWeather={fetchData} />
-      <Weather
-        description={weather.description}
-        city={weather.city}
-        country={weather.country}
-        error={weather.error}
-        temperature={weather.temperature}
-      />
-      {console.log('weather', weather)}
-      {console.log('weather.data', weather.data)}
+      <div className="wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="title-container">
+              <Titles />
+            </div>
+
+            <div className="form-container">
+              <Form getWeather={fetchData} />
+              <Weather
+                city={weather.city}
+                country={weather.country}
+                description={weather.description}
+                temperature={weather.temperature}
+                icon={weather.icon}
+                error={weather.error}
+              />
+              {console.log('weather.data', weather.data)}
+            </div>
+          </div>
+        
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Titles/Titles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Titles.scss';
+
+function Titles() {
+  return (
+    <div className="titles">
+      <h1 className="titles__title">Weather Finder</h1>
+      <p className="titles__subtitle">
+        Find temperature, conditions, and more...
+      </p>
+    </div>
+  );
+}
+
+export default Titles;
