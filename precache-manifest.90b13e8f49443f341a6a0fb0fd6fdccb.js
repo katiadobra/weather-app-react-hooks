ee1af11adaf67a0b5eceea8e764e7c9795f06a17
katiadobra <broun.kat@gmail.com>
2020-08-19 16:20:19 +0200

    Updates

---
+++ precache-manifest.90b13e8f49443f341a6a0fb0fd6fdccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533754960cea8e7ca469f15fafde3b7f",
+    "url": "/weather-app-react-hooks/index.html"
+  },
+  {
+    "revision": "2d2caef312cd433acb1c",
+    "url": "/weather-app-react-hooks/static/css/main.2dedc4f5.chunk.css"
+  },
+  {
+    "revision": "af4088d80b1f0de85c91",
+    "url": "/weather-app-react-hooks/static/js/2.58e9f96e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react-hooks/static/js/2.58e9f96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2caef312cd433acb1c",
+    "url": "/weather-app-react-hooks/static/js/main.d7f582e1.chunk.js"
+  },
+  {
+    "revision": "17a3a0f5de3acd5775b3",
+    "url": "/weather-app-react-hooks/static/js/runtime-main.4fdfbe02.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/weather-app-react-hooks/precache-manifest.90b13e8f49443f341a6a0fb0fd6fdccb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app-react-hooks/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
